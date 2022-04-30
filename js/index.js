d2873d0ce5dad0cb8292e530756c10b00571da4d
Savchenko Andrey <102185395+asavchenko14@users.noreply.github.com>
2022-04-30 23:13:54 +0300

    обновил данные

--- js/index.js
+++ js/index.js
@@ -1,18 +1,19 @@
 $(document).ready(function(){
-	let countBlockTop = $(".stat").offset().top; //смещение блока с числом относительно верхнего края
+	let countBlockTop = $(".stat2 p").offset().top; //смещение блока с числом относительно верхнего края
 	let show = true; // отвечает, что если один раз счетчик сработает, больше не срабатывал
 	let height = $(window).innerHeight(); //высота окна браузера
 	console.log(countBlockTop);
 	console.log(height);
 		$(window).scroll( function (){
 		 let top = $(window).scrollTop();	
-	console.log(top);
-			if(show && (countBlockTop < top + height)){ //логика и должны быть два элемента true
-				show = false; //если мы видим число, то больше его не надо показывать
-						
+			console.log(top);
+			if(show && (countBlockTop < (top + height))){ //логика и должны быть два элемента true
+				console.log('log');
+				show = false; //если мы видим число, то больше его не надо показывать		
 				$('.stat2 p').spincrement({ //вызов плагина с параметрами 
 					from: 1, //начинать с 1
 					duration: 4000, //задержка счетчика
+					
 				});
 			}
 		});
