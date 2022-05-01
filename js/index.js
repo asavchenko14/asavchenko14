ee39f973b54b9928abb5fad91cb2b16906f58748
Savchenko Andrey <102185395+asavchenko14@users.noreply.github.com>
2022-05-01 19:59:51 +0300

    Добавил слайды для кейса

--- js/index.js
+++ js/index.js
@@ -26,6 +26,37 @@ nav.find('.a').on('click', function () {
   
   return false;
 });
+	
+	//начало блока слайдера для кейса
+let viewport = $("#viewport").width();
+let slider = $("div.slider");
+let viewSliders = $(".viewSlide");
+let viewSlide = 0;
+
+viewSliders[viewSlide].style.backgroundColor = "green";
+
+$(".next").click(function () { 
+    viewSliders[viewSlide].style.backgroundColor = "red";
+    if (viewSlide < 4) {
+        viewSlide++;
+    } else {
+        viewSlide = 0;
+    }
+    viewSliders[viewSlide].style.backgroundColor = "green";
+    slider.animate({'left': -viewSlide * viewport + "px"}, {'duration': 500})  
+});
+
+$(".prev").click(function () { 
+    viewSliders[viewSlide].style.backgroundColor = "red";
+    if (viewSlide > 0) {
+        viewSlide--;
+    } else {
+        viewSlide = 4;
+    }
+    viewSliders[viewSlide].style.backgroundColor = "green";
+    slider.animate({'left': -viewSlide * viewport + "px"}, {'duration': 500})  
+});
+	
 //начало блока анимации цифр
 	let countBlockTop = $(".stat2 p").offset().top; //смещение блока с числом относительно верхнего края
 	let show = true; // отвечает, что если один раз счетчик сработает, больше не срабатывал
